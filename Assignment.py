74883b050e41ea77517f5b182cd42d8a92382f89
jbl998 <jbl998@alumni.ku.dk>
2022-06-06 18:38:53 +0200

    Test

--- Assignment.py
+++ Assignment.py
@@ -1,14 +1,18 @@
 from inspect import _void
 import numpy as np
 from gurobipy import GRB, Model
-print("Assignment June 8th")
+print("Kristian Kofoed Mortensen (JBL998)\n"
+      "Advanced Operations Research \n"
+      "Practical assignment June 8th")
+# Setting seed to ensure replicability
+np.random.seed(10)
 
 ##### TASK 2 #####
 
 def TaskTwoUpperBound(M,K)->(_void):
     objV=np.zeros(M)    # Object value vector
-    c1=2; c2=4; c3=5.2  # Cost of procurement
-    q1=60; q2=40; q3=10 # Selling price
+    c1 = 2; c2 = 4; c3 = 5.2  # Cost of procurement
+    q1 = 60; q2 = 40; q3 = 10 # Selling price
     for m in range(M):
         # Creating the model
         Prod = Model("Production")
@@ -41,25 +45,25 @@ def TaskTwoUpperBound(M,K)->(_void):
 
         # Optimizing
         Prod.optimize(callback=None)
-        objV[m]=Prod.ObjVal
+        objV[m] = Prod.ObjVal
 
     U_KM = np.mean(objV)
     sigma_U = np.sqrt(sum([np.power(objV[m]-U_KM,2) for m in range(M)])/(M-1))
-    print('M =',M,', K =',K,'Upper bound with 95% confidence bands:', U_KM+(sigma_U*1.96/np.sqrt(M))*np.array([-1,0,1]))
+    print('M =', M ,', K =', K ,'Upper bound with 95% confidence bands:',
+          U_KM+(sigma_U*1.96/np.sqrt(M))*np.array([-1,0,1]))
     # Print for latex copy
     # print('$',round(U_KM,2),'\pm',round(sigma_U*1.96/np.sqrt(M),2),'$\n')
 
 # Getting upper bounds for different values of M and K
-np.random.seed(10)
 TaskTwoUpperBound(5, 5)
-TaskTwoUpperBound(5, 50)
-TaskTwoUpperBound(5, 500)
-TaskTwoUpperBound(50, 5)
-TaskTwoUpperBound(50, 50)
-TaskTwoUpperBound(50, 500)
-TaskTwoUpperBound(500, 5)
-TaskTwoUpperBound(500, 50)
-TaskTwoUpperBound(500, 500)
+# TaskTwoUpperBound(5, 50)
+# TaskTwoUpperBound(5, 500)
+# TaskTwoUpperBound(50, 5)
+# TaskTwoUpperBound(50, 50)
+# TaskTwoUpperBound(50, 500)
+# TaskTwoUpperBound(500, 5)
+# TaskTwoUpperBound(500, 50)
+# TaskTwoUpperBound(500, 500)
 
 ##### TASK 3 #####
 
@@ -100,7 +104,6 @@ def TaskThreeApproximation(K)->(_void):
     print('K =', K,' Optimum: x1 =', round(x1.x,2),'x2 =', round(x2.x,2),'x3 =', round(x3.x,2),
           'Objective:', round(prod.objVal, 2))
 
-np.random.seed(10)
 # TaskThreeApproximation(5)
 # TaskThreeApproximation(50)
 # TaskThreeApproximation(500)
@@ -180,14 +183,13 @@ def EEV(K)->(_void):
     prod.addConstrs(y2[k] <= d2[k] for k in range(K))   # Produce less than demand, product 2
     prod.addConstrs(y3[k] <= d3[k] for k in range(K))   # Produce less than demand, product 3
     prod.addConstr(x1 == 2100)
-    prod.addConstr(x2 == 925)
-    prod.addConstr(x3 == 500)
+    prod.addConstr(x2 == 900)
+    prod.addConstr(x3 == 525)
 
     # Optimizing
     prod.optimize()
     print('EEV: K =', K, round(prod.objVal, 2))
 
-np.random.seed(10)
 # EEV(5)
 # EEV(50)
 # EEV(500)
