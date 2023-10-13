305c0180c5120fae3ad66f80c9cec34100e6fc05
jbl998 <jbl998@alumni.ku.dk>
2023-10-13 13:29:15 +0200

    Added split calculation Changed order of tabs

--- running.py
+++ running.py
@@ -4,7 +4,7 @@ import numpy as np
 import datetime
 
 
-tab1, tab2 = st.tabs(["Pace to Race Time", "Race time to pace"])
+tab1, tab2 = st.tabs(["Race time to pace", "Pace to Race Time"])
 
 now = datetime.datetime.now()
 d = now.day
@@ -21,22 +21,55 @@ def pacetotime(min, sec, dist):
 def timetopace(hrs, min, sec, dist):
     total_seconds = hrs * 3600 + min * 60 + sec
     secperkm = total_seconds / (dist/1000)
-    m, s = divmod(secperkm, 60)
-    return int(m), np.round(s,1)
+    h, rest = divmod(secperkm, 3600)
+    m, s = divmod(rest, 60)
+    s2, mys = divmod(s,1)
+    return int(h), int(m), int(s2), int(mys*1000000)
 
 
-dist = [1000,
-        5000,
-        10000,
-        21097.5,
-        42195]
-record = [datetime.datetime(year=y, month=mo, day=d, hour=0, minute=4, second=33),  # 1000m
+dist_m = [1000, 5000, 10000, 21097.5, 42195]
+dist_km = [x/1000 for x in dist_m]
+record = [datetime.datetime(year=y, month=mo, day=d, hour=0, minute=4, second=29),  # 1000m
           datetime.datetime(year=y, month=mo, day=d, hour=0, minute=23, second=46),  # 5000m
-          datetime.datetime(year=y, month=mo, day=d, hour=0, minute=55, second=13),  # 10000m
+          datetime.datetime(year=y, month=mo, day=d, hour=0, minute=53, second=57),  # 10000m
           datetime.datetime(year=y, month=mo, day=d, hour=6, minute=0, second=0),  # Half
           datetime.datetime(year=y, month=mo, day=d, hour=6, minute=0, second=0)]   # Marathon
+record2 = [item.strftime("%H:%M:%S") for item in record]
+
 
 with tab1:
+    st.subheader("Time")
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        h = st.number_input(label="H", min_value=0, max_value=23, value=3)
+    with col2:
+        m = st.number_input(label="M", min_value=0, max_value=59, value=39)
+    with col3:
+        s = st.number_input(label="S", min_value=0, max_value=59, value=59)
+    time = datetime.time(hour=h, minute=m, second=s)
+    st.subheader("Pace to run")
+    d_choice_km = st.radio("Choose distance", options=dist_km, index=len(dist_km)-1)
+    d_choice_m = int(d_choice_km*1000)
+    hour, min, sec, mys = timetopace(hrs=h, min=m, sec=s, dist=d_choice_m)
+    timezero = datetime.datetime(year=y, month=mo, day=d, hour=0, minute=0, second=0, microsecond=0)
+    pace = datetime.datetime(year=y, month=mo, day=d, hour=hour, minute=min, second=int(sec), microsecond=mys)
+    delta = datetime.timedelta(hours=hour, minutes=min, seconds=sec, microseconds=mys)
+    st.text(f"This will require you to run at pace {pace.strftime('%M:%S.%f')[:10]}")
+
+
+    st.subheader('Split times')
+    d_split = st.radio("Choose split (km)", options=[1,2,5], index=0)
+    antal, rest = divmod(d_choice_m, 1000*d_split)
+    d_range = [d_split*x for x in range(1, antal+1)]
+
+    t_range = [(timezero+d_split*x*delta).strftime("%H:%M:%S.%f")[:10] for x in range(1, antal+1)]
+    if rest > 0:
+        d_range.append(d_choice_km)
+        t_range.append(time.strftime("%H:%M:%S.%f")[:10])
+    data_pace = pd.DataFrame(data={"Distance (km)": d_range, "Time (HH:MM:SS.N)": t_range})
+    st.dataframe(data_pace, hide_index=True, use_container_width=True)
+
+with tab2:
     st.subheader("Pace")
     col1, col2 = st.columns(2)
     with col1:
@@ -48,26 +81,16 @@ with tab1:
 
     st.subheader("Time to run")
 
-    time = [pacetotime(minutes, seconds, item) for item in dist]
-    d = {"Distance": dist, "Time": time, "Record": record}
+    time = [pacetotime(minutes, seconds, item) for item in dist_m]
 
-    data = pd.DataFrame(data=d, columns=["Distance", "Time", "Record"])
-    st.dataframe(data)
+    data = pd.DataFrame(data={"Distance": dist_m, "Time": time, "Record": record},
+                        columns=["Distance", "Time", "Record"])
+
+    time2 = [item.strftime("%H:%M:%S") for item in time]
+    data2 = pd.DataFrame(data={"Distance": dist_m, "Time": time2, "Record": record2},
+                         columns=["Distance", "Time", "Record"])
+
+    st.dataframe(data2)
 
     # Plots
     st.line_chart(data=data, x="Distance")
-
-with tab2:
-    st.subheader("Time")
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        h = st.number_input(label="H", min_value=0, max_value=23, value=0)
-    with col2:
-        m = st.number_input(label="M", min_value=0, max_value=59, value=25)
-    with col3:
-        s = st.number_input(label="S", min_value=0, max_value=59, value=0)
-
-    st.subheader("Pace to run")
-    distance = st.radio("Choose distance", options=dist)
-    min, sec = timetopace(hrs=h, min=m, sec=s, dist=distance)
-    st.text(f"This will require you to run at pace {min}:{sec}")
